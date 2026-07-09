3db218315c777f4ce6a9b7c91388d6e25686be29
Карелин Егор Денисович <E.Karelin@local.as.ntl.ru>
2026-07-09 12:53:52 +0300

    Remove unsupported eslint key from next.config

--- next.config.js
+++ next.config.js
@@ -6,9 +6,6 @@ const nextConfig = {
     // Включаем игнор, чтобы сайт собирался и работал.
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
